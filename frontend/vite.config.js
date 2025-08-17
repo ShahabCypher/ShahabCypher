edcb12963b4cd3370a6a9606b275227849544537
Shahab Cypher <shahabcypher@gmail.com>
2025-08-17 20:11:55 +0330

    added absolute paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      contexts: "/src/contexts",
+      hooks: "/src/hooks",
+      layout: "/src/layout",
+      utils: "/src/utils",
+      assets: "/src/assets",
+      pages: "/src/pages",
+      services: "/src/services",
+    },
+  },
 });
