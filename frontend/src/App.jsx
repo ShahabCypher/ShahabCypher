5ed72d82fa2cf7ed24a6b607540dc7001360a6de
Shahab Cypher <shahabcypher@gmail.com>
2025-08-18 04:14:26 +0330

    add routes and header base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import Layout from "layout/Layout";
 import ThemeProvider from "contexts/ThemeContext";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Layout>
-        <h1>Cypher</h1>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
       </Layout>
     </ThemeProvider>
   );
--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Moon, Sun } from "lucide-react";
+
+import { useTheme } from "hooks/useTheme";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <nav>
+      <h1>Cypher</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <button onClick={toggleTheme}>
+        {theme === "dark" ? <Sun /> : <Moon />}
+      </button>
+    </nav>
+  );
 };
 
 export default Header;
