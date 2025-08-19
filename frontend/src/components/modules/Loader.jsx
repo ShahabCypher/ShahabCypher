0140af15a4caa54b7a10a0922043b07f1eb75cb6
Shahab Cypher <shahabcypher@gmail.com>
2025-08-19 04:52:39 +0330

    added loader

---
+++ frontend/src/components/modules/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-10 h-10 border-t-4 border-b-4 border-gray-900 dark:border-gray-100 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
 
+import Loader from "src/components/modules/Loader";
+
 const Layout = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen max-w-screen-2xl mx-auto">
-      <Header />
-      <main className="flex-grow">{children}</main>
-      <Footer />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
