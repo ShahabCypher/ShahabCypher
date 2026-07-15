aad773d428f2f192813bc5a48038337021e1d386
Shahab Cypher <shahabcypher@gmail.com>
2026-07-15 19:09:36 +0330

    added check path to api

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.route("/api/sendMessage").post(async (req, res) => {
   }
 });
 
+app.get("/api/check", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
